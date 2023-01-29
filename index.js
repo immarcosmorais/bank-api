4c03026ac256c1e94ecdb34c1602d1f11e8aa64c
Marcos Morais <marcosmorais.contact@gmail.com>
2023-01-29 17:00:43 -0300

    Configurado graphql

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import winston from "winston";
 import accountRouter from "./routes/account.routes.js";
 import { promises as fs } from "fs";
 import cors from "cors";
+import { buildSchema } from "graphql";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 const { readFile, writeFile } = fs;
@@ -21,9 +23,30 @@ global.logger = winston.createLogger({
   format: combine(label({ label: "my-bank-api" }), timestamp(), myFormat),
 });
 
+const schema = buildSchema(`
+  type Account {
+    id: Int
+    name: String
+    balance: Float
+  }
+  type Query{
+    all: [Account]
+    byId(id: Int): Account
+  }
+`);
+
 app.use(express.json());
 app.use(cors());
 app.use("/account", accountRouter);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: null,
+    graphiql: true,
+  })
+);
 
 app.listen(3000, async () => {
   try {
